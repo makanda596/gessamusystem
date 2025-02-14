c85f278f56fc7f297cfc9c9532249e78a416ca29
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-14 09:30:18 +0300

    D

--- server/utilis/UsersetCookieGenerateToken.js
+++ server/utilis/UsersetCookieGenerateToken.js
@@ -11,7 +11,7 @@ export const UsersetCookieGenerateToken = async (res, userId) => {
         res.cookie("token", token, {
             httpOnly: true,
             sameSite: "Strict",
-            secure: process.env.NODE_ENV === "production",
+            secure: true,
             // maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days in milliseconds
         })
         if (!token) {
