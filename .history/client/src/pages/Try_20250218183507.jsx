c345a148bdf08b2dbbae06bb26f0187fd48a8289
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-18 19:20:20 +0300

    S

---
+++ .history/client/src/pages/Try_20250218183507.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Try = () => {
+  return (
+    <>
+    <Navbar/>
+    <div>Try</div>
+      </>
+  )
+}
+
+export default Try
