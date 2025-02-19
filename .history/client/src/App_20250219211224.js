861360b2f670d64ecd9f0f246ed71b727727af7e
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-19 21:12:43 +0300

    s

--- .history/client/src/App_20250219211224.js
+++ .history/client/src/App_20250219211224.js
@@ -95,7 +95,7 @@ function App() {
           <Route path="/alert" element={<ProtectedRoute><Alert userId="67a20fc8b0d2021b64dc6466" /></ProtectedRoute>} />
           <Route path="/task/:id" element={<ProtectedRoute><Specific /></ProtectedRoute>} />
           <Route path="/trainings" element={<ProtectedRoute><Training /></ProtectedRoute>} />
-          <Route path="/asqQuiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
+          <Route path="/Qasquiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
           <Route path="/project/:id" element={<ProtectedRoute><OneProject /></ProtectedRoute>} />
           <Route path="/adminDashboard" element={<AdminDashboard />} />
           {/* <Route path="*" element={<h1>404 - Page Not Found</h1>} /> */}
