e3db5fabf919e639f96242640d95f8604edb55d8
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-21 08:49:27 +0300

    D

--- .history/server/utilis/AdminsetCookieGenerateToken_20250221083857.js
+++ .history/server/utilis/AdminsetCookieGenerateToken_20250221083857.js
@@ -4,22 +4,23 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const SECTRET_KEY = process.env.SECTRET_KEY
-export const setCookieGenerateToken = async (res, adminId) => {
+export const AdminsetCookieGenerateToken = async (res, adminId) => {
 
     try {
-        const token = jwt.sign({ adminId }, SECTRET_KEY, { expiresIn: "1m" })
+        const token = jwt.sign({ adminId }, SECTRET_KEY, { expiresIn: "1day" })
 
         res.cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+          httpOnly: true,
+            secure: true,
+            sameSite:"None",
             maxAge: 4 * 24 * 60 * 60 * 1000,
         })
         if (!token) {
             res.status(404).json({ message: "token not provided" })
+            return 0;
         }
         return token
     } catch (error) {
-        res.status(500).json({ message: "Internal server error", error: error.message });
+        res.status(404).json(error.message)
     }
 }
