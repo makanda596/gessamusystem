aa2ef68a7cfb7dfb6a03d45a0cf50630f8314ff6
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-21 06:15:12 +0300

    D

--- .history/client/src/App_20250221060959.js
+++ .history/client/src/App_20250221060959.js
@@ -65,10 +65,9 @@ function App() {
   // Checking authentication - show loading screen
   if (isCheckingAuth) {
     return (
-      <>h</>
-      // <div className="flex items-center justify-center min-h-screen">
-      //   <div className="w-16 h-16 border-4 border-t-red-600 border-b-green-600 border-l-white border-r-white rounded-full animate-spin"></div>
-      // </div>
+      <div className="hidden flex items-center justify-center min-h-screen">
+        <div className="w-16 h-16 border-4 border-t-red-600 border-b-green-600 border-l-white border-r-white rounded-full animate-spin"></div>
+      </div>
     );
   } else {
     // Main app content after auth check
