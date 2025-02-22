be36cb154bc050247eb63f150fca518b523ae6f9
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-22 09:12:03 +0300

    D

--- client/src/pages/ADMIN/Student.jsx
+++ client/src/pages/ADMIN/Student.jsx
@@ -13,7 +13,7 @@ const Student = () => {
     // Fetch students from backend
     const fetchStudents = async () => {
         try {
-            const response = await axios.get("http://localhost:5000/auth/getStudents");
+            const response = await axios.get("https://gessamubackend.onrender.com/auth/getStudents");
             setStudents(response.data);
             setFilteredStudents(response.data);
         } catch (error) {
