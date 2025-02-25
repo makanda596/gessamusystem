7d2aedd2219b0be86cde2796edf8c386ae16d080
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-25 10:43:18 +0300

    g

--- .history/client/src/components/Navbar_20250225104155.jsx
+++ .history/client/src/components/Navbar_20250225104155.jsx
@@ -89,7 +89,7 @@ const Navbar = ({ userId }) => {
 
         {/* Mobile Menu Button */}
         <label htmlFor="menu-toggle" className="lg:hidden cursor-pointer">
-          <FaBars className="text-white text-2xl"/>
+          <FaBars className="text-white text-2xl" />
         </label>
       </nav>
 
@@ -105,7 +105,7 @@ const Navbar = ({ userId }) => {
         </div>
 
         {/* Mobile Navigation */}
-        <nav className="flex flex-col space-y-4  mt-6">
+        <nav className="flex flex-col space-y-4 z-40 mt-6">
           <a href="/dashboard" className="hover:text-gray-300 transition duration-300 ease-in-out hover:underline">
             Home
           </a>
