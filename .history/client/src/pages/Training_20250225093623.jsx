b9afae3cc3530955e8a88dce6ed8fbed38a2e563
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-25 09:39:14 +0300

    g

--- .history/client/src/pages/Training_20250225093623.jsx
+++ .history/client/src/pages/Training_20250225093623.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "./Try";
 
 const trainingSessions = [
   { id: 1, title: "GIS Basics", date: "2025-03-01", status: "Upcoming" },
@@ -18,7 +18,7 @@ const Training = () => {
 
   return (
     <>
-    <Navbar className="z-index"/>
+    <Navbar/>
     <div className="max-w-4xl mx-auto p-6">
       <h2 className="text-2xl font-bold text-center text-blue-700 mb-4">Training Sessions</h2>
 
