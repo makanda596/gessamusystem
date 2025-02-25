122c976b163f5cda3b8c268bcc518f7d7fe60fb0
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-25 05:15:58 +0300

    a

--- .history/client/src/pages/Tasks_20250225051501.jsx
+++ .history/client/src/pages/Tasks_20250225051501.jsx
@@ -70,7 +70,7 @@ const Tasks = () => {
                     <div className="text-center text-gray-600">No tasks found.</div>
                 ) : (
                     <div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         {filteredTasks.map((task) => (
                             <TaskCard key={task._id} task={task} showNotification={showNotification} />
                         ))}
