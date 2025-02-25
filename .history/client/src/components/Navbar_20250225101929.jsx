eb22980ce670be8abfd7d61301ef18628d1ef7a8
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-25 10:24:59 +0300

    g

--- .history/client/src/components/Navbar_20250225101929.jsx
+++ .history/client/src/components/Navbar_20250225101929.jsx
@@ -95,8 +95,8 @@ const Navbar = ({ userId }) => {
 
       {/* Mobile Sidebar */}
       <input type="checkbox" id="menu-toggle" className="hidden peer" />
-      <div className="fixed inset-0 bg-black bg-opacity-50 hidden peer-checked:block z-10 lg:hidden"></div>
-      <div className="fixed top-0 left-0 h-full w-64 bg-green-900 text-white p-5 pt-14 transform -translate-x-full peer-checked:translate-x-0 transition-transform duration-300 ease-in-out shadow-lg lg:hidden">
+      <div className="fixed inset-0 bg-black bg-opacity-50 hidden peer-checked:block lg:hidden"></div>
+      <div className="fixed top-0 left-0 h-full w-64 bg-green-900 text-white p-5 transform -translate-x-full peer-checked:translate-x-0 transition-transform duration-300 ease-in-out shadow-lg lg:hidden">
         <div className="flex justify-between items-center mb-6">
           <h4 className="text-xl font-bold">Menu</h4>
           <label htmlFor="menu-toggle" className="cursor-pointer">
@@ -105,7 +105,7 @@ const Navbar = ({ userId }) => {
         </div>
 
         {/* Mobile Navigation */}
-        <nav className="flex flex-col space-y-4 z-40 mt-6">
+        <nav className="flex flex-col space-y-4 z-40">
           <a href="/dashboard" className="hover:text-gray-300 transition duration-300 ease-in-out hover:underline">
             Home
           </a>
